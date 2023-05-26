90fc044283ee9d00b0aedf6764012d1f2e523c0e
Junee <juneekim7@gmail.com>
2023-05-26 17:21:06 +0900

    error

--- src/algorithm.ts
+++ src/algorithm.ts
@@ -1,4 +1,21 @@
+// every function must be pure function
+
+function isConnected() {
+    return true
+}
+
+function isCactus() {
+    return true
+}
+
+function isCycle() {
+
+}
+
 function getEccSeq() {
+    if (!isConnected()) throw new Error('The graph must be connected')
+    if (!isCactus()) throw new Error('The graph must be a cactus graph!')
+
     return null
 }
 
